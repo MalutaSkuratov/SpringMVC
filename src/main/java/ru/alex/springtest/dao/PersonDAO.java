60008ab22fd8387efa1b9d7a1f2b2a1865ee53e6
Alexandr <narvyshalexandr@gmail.com>
2022-09-08 17:06:11 +0300

    POST method. Add person

--- src/main/java/ru/alex/springtest/dao/PersonDAO.java
+++ src/main/java/ru/alex/springtest/dao/PersonDAO.java
@@ -1,4 +1,36 @@
 package ru.alex.springtest.dao;
 
+import org.springframework.stereotype.Component;
+import ru.alex.springtest.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PersonDAO {
+    private static int PEOPLE_COUNT;
+
+    private List<Person> people;
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Alex"));
+        people.add(new Person(++PEOPLE_COUNT,"Max"));
+        people.add(new Person(++PEOPLE_COUNT,"Jim"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+
 }
--- src/main/java/ru/alex/springtest/model/Person.java
+++ src/main/java/ru/alex/springtest/model/Person.java
@@ -1,4 +1,32 @@
 package ru.alex.springtest.model;
 
 public class Person {
+    private int id;
+    private String name;
+
+
+    public Person(){
+
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
